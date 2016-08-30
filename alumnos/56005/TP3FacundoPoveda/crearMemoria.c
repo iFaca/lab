67f945b5284733f89e08cbe2ac8cfc2c5723178f
Facundo Poveda <facapoveda@gmail.com>
2016-08-30 12:32:44 -0300

    Mejora para archivo de cualquier tamaño

--- alumnos/56005/TP3FacundoPoveda/crearMemoria.c
+++ alumnos/56005/TP3FacundoPoveda/crearMemoria.c
@@ -8,50 +8,61 @@ int crearMemoria (char * archivo){
 	char *charMmap;
 	int *intMmap;
 	int i;
+	int cantPasadas = 0;
 	pid_t pid;
-        sem_t *semaforo;
+	sem_t *semaforo;
+	//sem_t *semaforo2;
 
 	//Abrimos el arhivo que le pasamos como parámetro
 	file = open(archivo, O_RDWR);
-	
-	//Leemos el archivo para ver su tamaño
-	fileSize = read (file,buffer,sizeof(buffer));
+
+	//Creamos el primer semáforo
+	//semaforo2 = sem_open("SemaforoB", O_CREAT , 0666 , 0);
+
+	//Vamos leyendo partes del archivo
+	while ((fileSize = read (file,buffer,sizeof(buffer))) == BUFFER){
+		cantPasadas++;	
+		fileSize += fileSize; 
+	}
+
+	//Calculamos el tamaño del archivo multiplicando la cantidad de pasadas por las veces que se llenó el buffer
+	fileSize = fileSize + BUFFER * cantPasadas;
 	printf("Tamaño del archivo %d \n",fileSize);
 
 	//Creamos un espacio de memoria compartida para guardar el texto del buffer	
 	charMmap = mmap(NULL, fileSize , PROT_READ | PROT_WRITE , MAP_SHARED|MAP_ANONYMOUS , -1 , 0);
 	//Creamos otro espacio de memoria compartida para guardar el arreglo con la cantidad de letras por palabra
 	intMmap = mmap(NULL, sizeof(int)*15, PROT_READ | PROT_WRITE , MAP_SHARED|MAP_ANONYMOUS , -1 , 0);	
-	
-    	//Cerramos el descriptor
-    	if (close (file) == -1) {
-      	printf("Error al cerrar el archivo\n");
-      	return 1;   
-    	}
-	
+
+	//Cerramos el descriptor
+	if (close (file) == -1) {
+		printf("Error al cerrar el archivo\n");
+		return 1;   
+	}
+
 	//Escribimos en la memoria compartida
 	for (i=0 ; i < fileSize ; i++){
-	*(charMmap+i) = buffer[i];	
+		*(charMmap+i) = buffer[i];	
 	}	
-	
-	//Creamos el semaforo
-	semaforo = sem_open("Semaforo", O_CREAT , 0666 , 0);
-		
-	//Creamos el hijo
-        pid = fork();
 
-        switch(pid){
+	//Creamos el semaforo
+	semaforo = sem_open("SemaforoA", O_CREAT , 0666 , 0);
+
+	//Creamos el hijo
+	pid = fork();
+
+	switch(pid){
 		case -1:
 			//Error
 			perror("No se ha podido crear el proceso hijo \n");
 			break;
-        	case 0:
+		case 0:
 			//hijo
-        		printf("Soy el hijo: %d - Mi Padre es: %d \n", getpid(), getppid());
-			
+			printf("Soy el hijo: %d - Mi Padre es: %d \n", getpid(), getppid());
+
 			//Hacemos el conteo de las palabras y letras del archivo
 			conteo( fileSize , charMmap , intMmap);
-			
+
 			//Le sumamos 1 al semáforo
 			sem_post(semaforo);
 			break;
@@ -59,14 +70,14 @@ int crearMemoria (char * archivo){
 			//Le restamos 1 al semáforo
 			sem_wait(semaforo);
 
-        		//padre
-        		printf("Soy el padre: %d \n", getpid());
-			
+			//padre
+			printf("Soy el padre: %d \n", getpid());
+
 			//Leemos el conteo de la memoria compartida y lo imprimimos por pantalla
-                        for (i = 0; i < 15 ; i++){
-                        printf("Palabras de longitud %d = %d \n", i+1, *(intMmap+i));
-                        }
-       			break;
+			for (i = 0; i < 15 ; i++){
+				printf("Palabras de longitud %d = %d \n", i+1, *(intMmap+i));
+			}
+			break;
 	}
 	return 0;
 }
